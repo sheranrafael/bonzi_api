c797b781da45133cb05c4f5117a79025435265df
sheran <sheranv@proton.me>
2025-09-18 12:10:11 -0300

    validações melhoradas e logs adicionados

--- app/controllers/transacoes_controller.rb
+++ app/controllers/transacoes_controller.rb
@@ -1,45 +1,62 @@
 class TransacoesController < ApplicationController
-  # Armazenamento em memória
   @@transacoes = []
   @@mutex = Mutex.new
 
   # POST /transacao
   def create
-    transacao_params = params.permit(:valor, :dataHora)
-    
-    # Validar JSON
-    unless transacao_params[:valor] && transacao_params[:dataHora]
-      return render status: :bad_request
-    end
+    begin
+      # Ler e parsear o JSON
+      json_data = JSON.parse(request.body.read)
+      
+      # Validar campos obrigatórios
+      unless json_data['valor'] && json_data['dataHora']
+        Rails.logger.warn "Campos obrigatórios faltando: #{json_data}"
+        return render status: :bad_request
+      end
 
-    # Validar tipos
-    valor = transacao_params[:valor].to_f
-    data_hora = DateTime.parse(transacao_params[:dataHora]) rescue nil
-    
-    unless data_hora
-      return render status: :bad_request
-    end
+      # Validar e converter tipos
+      valor = json_data['valor'].to_f
+      data_hora = DateTime.parse(json_data['dataHora']) rescue nil
+      
+      unless data_hora
+        Rails.logger.warn "Data/Hora inválida: #{json_data['dataHora']}"
+        return render status: :bad_request
+      end
 
-    # Validar regras de negócio
-    if valor < 0 || data_hora > DateTime.now
-      return render status: :unprocessable_entity
-    end
+      # Validar regras de negócio
+      if valor < 0
+        Rails.logger.warn "Valor negativo não permitido: #{valor}"
+        return render status: :unprocessable_entity
+      end
 
-    # Salvar transação
-    @@mutex.synchronize do
-      @@transacoes << {
-        valor: valor,
-        dataHora: data_hora
-      }
-    end
+      if data_hora > DateTime.now
+        Rails.logger.warn "Data futura não permitida: #{data_hora}"
+        return render status: :unprocessable_entity
+      end
 
-    render status: :created
+      # Salvar transação
+      @@mutex.synchronize do
+        @@transacoes << {
+          valor: valor,
+          dataHora: data_hora
+        }
+        Rails.logger.info "Transação salva: valor=#{valor}, data=#{data_hora}"
+      end
+
+      render status: :created
+
+    rescue JSON::ParserError
+      Rails.logger.error "JSON inválido recebido"
+      render status: :bad_request
+    end
   end
 
   # DELETE /transacao
   def destroy
     @@mutex.synchronize do
+      count = @@transacoes.size
       @@transacoes.clear
+      Rails.logger.info "Todas as #{count} transações foram removidas"
     end
     
     render status: :ok
